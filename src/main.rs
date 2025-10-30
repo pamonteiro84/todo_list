6fae26865092060750be561910fcc54f7153815f
Paulo Monteiro <paulomonteiro@MacBook-Pro-de-Paulo.local>
2025-10-30 01:02:10 +0000

    migrations done (fix error in main and make file)

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
-fn main() {
-    println!("Hello, world!");
+use sea_orm::Database;
+use dotenvy::dotenv;
+use std::env;
+
+
+
+#[tokio::main]
+async fn main() {
+
+    dotenv().ok();
+    let database_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
+
+    let db = Database::connect(&database_url).await.expect("Failed to connect to the database");
+
+    let _generate_entities = migration::Migrator::up(&db, None).await?;
+
+  
 }
